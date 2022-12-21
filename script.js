204db00685bea86953d3c74a4ac0e4757b60c9b2
Akhil chandra <akhilchandra2309@gmail.com>
2022-12-21 10:36:16 +0530

    solution

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
 //your code here
 const url = "https://randomuser.me/api/"
-// const fetchAPI = async fetch(url)
 
-let response=""
-let requiredData = ""
-let ageButton =  document.getElementById("age-button")
-const emailButton = document.getElementById("email-button")
-const phoneButton = document.getElementById("phone-button")
-
-const ageDisplay = ()=>{
-    // clear()
-    document.getElementById("para").innerHTML = ""
-    document.getElementById("para").innerHTML = `Age is ${requiredData.dob.age}`
-}
-
-const emailDisplay = ()=>{
-    document.getElementById("para").innerHTML = ""
-    document.getElementById("para").innerHTML = `Email is ${requiredData.email}`
-}
-
-const phoneDisplay = ()=>{
-    document.getElementById("para").innerHTML = ""
-    document.getElementById("para").innerHTML = `phone is ${requiredData.phone}`
-}
+// let response=""
+let requiredUser = {}
+const ageButton =  document.querySelector('[data-attr = "age"]')
+const emailButton = document.querySelector('[data-attr = "email"]')
+const phoneButton = document.querySelector('[data-attr = "phone"]')
+const getUser = document.querySelector("#getUser")
+let Username = document.querySelector("#name")
+let image = document.querySelector(".profile-pic")
+// let info = document.querySelector("#getUser")
+let additionalInfo = document.querySelector(".additionalInfo")
 
 const getApi = async()=>{
-    response = await fetch(url)
-    data = await response.json()
-    // console.log(data.results[0])
-    requiredData = data.results[0]
-    console.log(requiredData)
+    try{
+    const response = await fetch(url)
+    const data = await response.json()
+    requiredUser = data.results[0]
+    console.log(requiredUser)
+    
+} catch(error){
+    alert(error);
+    console.log(error)
+}
+}
+getApi()
+
+const photoNameDisplay = async() =>{
+    await getApi();
+    Username.textContent = `Username is: ${requiredUser.name.title} ${requiredUser.name.first} ${requiredUser.name.last}`
+    image.src = requiredUser.picture.large
+    additionalInfo.textContent=""
+}
 
 
 
-ageButton.onclick=ageDisplay
-emailButton.onclick=emailDisplay
-phoneButton.onclick=phoneDisplay
+//writing functions for items to be displayed.
+
+function getAdditionalInfo(e) {
+    const att = e.target.getAttribute("data-attr")
+    if (att == "age") {
+        additionalInfo.textContent = requiredUser.dob.age
+    } else if (att == "phone") {
+        additionalInfo.textContent = requiredUser.phone
+    } else if (att == "email") {
+        additionalInfo.textContent = requiredUser.email
+    }
+}
+// photoNameDisplay()
+
+ageButton.onclick= getAdditionalInfo
+emailButton.onclick = getAdditionalInfo
+phoneButton.onclick = getAdditionalInfo
+getUser.onclick = photoNameDisplay
+
+
+// ageButton.onclick=ageDisplay,
+// emailButton.onclick=emailDisplay
+// phoneButton.onclick=phoneDisplay
+// getUserButton.onclick=getApi
 
 // let fullName = requiredData['name'].first + requiredData['name'].last
 
@@ -43,49 +66,10 @@ phoneButton.onclick=phoneDisplay
 
 
 
-} 
-getApi()
-
-
-//making another api call
-const getUserButton = document.getElementById("getUser")
-let Newresponse=""
-let Newdata=""
-let requiredNewData=""
-const getNewData = async()=>{
-    Newresponse = await fetch("https://randomuser.me/api/")
-    Newdata = await Newresponse.json()
-    requiredNewData = Newdata.results[0]
-    console.log(requiredNewData, 'lowdaaaaaaa')
-    let fullName=requiredNewData['name'].first + requiredNewData['name'].last
-    console.log(fullName);
-
-    const nameDisplay = ()=>{
-        document.getElementById("para").innerHTML = ""
-        document.getElementById("para").innerHTML = `Name is ${fullName}`
-    }
-    nameDisplay()
+// getApi()
 
 
 
-    const photoDisplay = () =>{
-        var img = document.createElement('img')
-        img.src=`${requiredNewData['picture'].large}`
-        // document.getElementByTagName('img').className="newClass"
-        console.log(img.src)
-        document.getElementById("section").appendChild(img)
-    }
-    photoDisplay()
-
-
-    //display their age and email
-
-    const ageDisplay = ()=>{
-        document.getElementById("para").innerHTML = ""
-        document.getElementById("para").innerHTML = `Age is ${requiredNewData.dob.age}`
-    }
-    ageButton.onclick=ageDisplay
-}
 
 
 
